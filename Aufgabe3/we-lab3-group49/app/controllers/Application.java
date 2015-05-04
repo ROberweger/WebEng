2b0f3ac8086c9fbe788d6713e2a44fdfd595408d
Roland Oberweger <roland.oberweger@gmail.com>
2015-05-04 12:06:45 +0200

    Added files

--- Aufgabe3/we-lab3-group49/app/controllers/Application.java
+++ Aufgabe3/we-lab3-group49/app/controllers/Application.java
@@ -3,6 +3,9 @@ package controllers;
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import at.ac.tuwien.big.we15.lab2.api.JeopardyFactory;
+import at.ac.tuwien.big.we15.lab2.api.JeopardyGame;
+import at.ac.tuwien.big.we15.lab2.api.impl.PlayJeopardyFactory;
 
 public class Application extends Controller {
 
@@ -11,6 +14,9 @@ public class Application extends Controller {
     }
 
     public static Result jeopardy() {
-    	return ok("bla");
+    	JeopardyFactory factory = new PlayJeopardyFactory("data.de.json");
+    	JeopardyGame game = factory.createGame("Testuser");
+    	String bla =game.getCategories().get(0).getQuestions().get(0).getText();
+    	return ok(bla);
     }
 }
