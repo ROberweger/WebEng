cea1a5615bd9526d605973a24c7cc0cadc72bb00
Johanna Donabauer <e1226287@student.tuwien.ac.at>
2015-05-06 22:58:04 +0200

    jeopardy, question, winner sollten fertig sein bei login noch nicht 100% bei register hab ich bis jetzt noch nichts geaendert

--- Aufgabe3/we-lab3-group49/app/controllers/Application.java
+++ Aufgabe3/we-lab3-group49/app/controllers/Application.java
@@ -1,8 +1,12 @@
 package controllers;
 
+//IMPORTS KONTROLLIEREN, es kann vorkommen das auf einmal die Views weg sind!!!
+//import views.html.[viewname]
 import play.mvc.Controller;
 import play.mvc.Result;
 import views.html.index;
+import views.html.jeopardy;
+import views.html.login;
 import at.ac.tuwien.big.we15.lab2.api.JeopardyFactory;
 import at.ac.tuwien.big.we15.lab2.api.JeopardyGame;
 import at.ac.tuwien.big.we15.lab2.api.impl.PlayJeopardyFactory;
@@ -16,7 +20,15 @@ public class Application extends Controller {
     public static Result jeopardy() {
     	JeopardyFactory factory = new PlayJeopardyFactory("data.de.json");
     	JeopardyGame game = factory.createGame("Testuser");
-    	String bla =game.getCategories().get(0).getQuestions().get(0).getText();
-    	return ok(bla);
+    	return ok(jeopardy.render(game));
+    }
+    
+    public static Result login(){
+    	//https://www.playframework.com/documentation/2.2.x/JavaGuide4
+    	//return ok (login.render(Form.form(User.class)));
+    }
+    
+    public static Result authenticate() {
+        //https://www.playframework.com/documentation/2.2.x/JavaGuide4
     }
 }
